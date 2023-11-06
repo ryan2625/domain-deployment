a58d8cfad3233c37885286b7a9ab4d5ad82c70dc
ryan2625 <ryanfreas19@augustana.edu>
2023-11-05 19:38:51 -0600

    Adding files

---
+++ data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        image : './images/flavors.webp',
+        title : "Flavor Store",
+        tech : "MERN Stack, CSS, React Bootstrap",
+        description : "A fully responsive app that supports CRUD operations and dynamically renders items from the backend. Mindful of SEO and accessibility, this website was made for National Louis University's coding challenge.",
+        link : "https://mern-flavor-site.onrender.com/",
+        github : "https://github.com/ryan2625/NLU-flavor-app"},
+    {
+        image : './images/boxing.webp',
+        title : "Cross-City Boxing",
+        tech : "Javascript, HTML, CSS",
+        description : "A fully responsive react app created based on a Figma design.",
+        link : "https://cross-city-boxing.vercel.app/",
+        github : "https://github.com/ryan2625/cross-city-boxing"},
+    {
+        image : './images/nexus.webp',
+        title : "Nexus Retailers",
+        tech : "React, CSS, Javascript, Bootstrap",
+        description : "A fully responsive react app created using bootstrap and the Intersection Observer API.",
+        link : "https://nexus-cars.vercel.app/",
+        github : "https://github.com/ryan2625/nexus-Cars"},
+    {
+        image : './images/personal.webp',
+        title : "Personal Website",
+        tech : "React, Javascript, CSS",
+        description : "One of my first web development projects. Creating this website was a special experience and my first attempt at building responsive websites from scratch. If you would like to get to know more about me and see more of my projects and research, visit here! ",
+        link : "https://ryan-dev.vercel.app/",
+        github : "https://github.com/ryan2625/personalWebsite"},
+]
---
+++ script.js
@@ -0,0 +1,72 @@
+import { data } from "./data.js";
+
+$(document).ready(function() {
+
+    data.forEach((element, index) => {
+        var img = $('<img>').attr('src', element.image).attr("loading", "lazy")
+        var individual = $('<div>').addClass('individual').addClass("hidden");
+        var divider = $('<div>').addClass('portfolio-divider');
+        var desc = $('<div>').addClass('descriptions');
+        var titles = $('<div>').addClass('titles');
+        var h1 = $('<h1>').text(element.title);
+        var h2 = $('<h2>').text(element.tech);
+        var p = $('<p>').text(element.description);
+        var buttons = $('<div>').addClass('buttons');
+        var a1 = $('<a>').attr('href', element.link).text('Live App').attr("target", "_blank");
+        var a2 = $('<a>').attr('href', element.github).text('Github').attr("target", "_blank");
+        individual.attr('id', index)
+        individual.append(img)
+        individual.append(desc)
+
+        desc.append(titles)
+        desc.append(p)
+        desc.append(buttons)
+
+        titles.append(h1)
+        titles.append(h2)
+
+        buttons.append(a1)
+        buttons.append(a2)
+
+        $(".portfolio-display").append(individual);
+        $(".portfolio-display").append(divider)
+
+    });
+
+    var img2 = $('<img>').attr('src', "./images/dumbbell.svg")
+    var img3 = $('<img>').attr('src', "./images/glove.svg")
+    var img4 = $('<img>').attr('src', "./images/car.svg")
+
+    var div1 = $('<div>').attr('id', 'blob');
+    var div2 = $('<div>').attr('id', 'blob2');
+    var div3 = $('<div>').attr('id', 'blob4');
+
+    $("#0").append(div1.append(img2))
+    $("#1").append(div2.append(img3))
+    $("#2").append(div3.append(img4))
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, {
+        rootMargin: "0px 0px -250px 0px" 
+    });
+
+    const hiddenElements = document.querySelectorAll(".hidden");
+
+    hiddenElements.forEach(element => {
+        observer.observe(element);
+    });
+
+    const navElements = document.querySelectorAll(".observer");
+    
+    setTimeout(() => {
+        $(".loading").addClass("none");
+        console.log("hello");
+        $("html").css("overflow-y", "scroll");
+    }, 2000);
+
+  });
