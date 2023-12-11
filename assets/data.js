bfd8782e41de6985b05766b256fa2bf068aa825d
ryan2625 <ryanfreas19@augustana.edu>
2023-12-11 01:27:45 -0600

    Testing updated website

--- assets/data.js
+++ assets/data.js
@@ -1,38 +1,73 @@
 export const data = [
     {
+        image : './assets/cryptoVerse.webp',
+        title : "crypto-verse",
+        tech : ["React", "SCSS"],
+        description : "A crypto app where you can view the latest market data for cryptocurrencies via CoinGecko's API. ",
+        link : "https://crypto-net-phi.vercel.app/",
+        github : "https://github.com/ryan2625/crypto-net",
+        svg: "./assets/bitcoin.svg",
+        svgClass:"top-left"
+    },
+    {
         image : './assets/salesSpherePreview.webp',
         title : "SalesSphere",
-        tech : "React, CSS",
+        tech : ["React", "CSS"],
         description : "A fully functional ecommerce website complete with many different products and features.",
         link : "https://simply-ecom.vercel.app/",
         github : "https://github.com/ryan2625/simply-ecom"
     },
     {
+        image: "./assets/nighthawk.webp",
+        title: "Nighthawk Fitness",
+        demo: {
+            gmail: "demo_fitness@gmail.com",
+            password: "demo123$"
+        },
+        tech: ["React", "CSS", "Javascript", "Firebase"],
+        description: "A full-stack fitness app that can track your weight, create and log your workouts, and track your calories. This app is responsive and also includes user authentication and supports CRUD operations.",
+        link: "https://fitness-dev-2.web.app/",
+        github: "https://github.com/AugustanaCSC490Spring23/Nighthawk-Fitness",
+        svg: "./assets/dumbbell.svg",
+        svgClass: "top-right"
+    },
+    {
         image : './assets/flavors.webp',
         title : "Flavor Store",
-        tech : "MERN Stack, CSS, React Bootstrap",
+        tech : ["MERN Stack", "CSS"],
         description : "A fully responsive app that supports CRUD operations and dynamically renders items from the backend. Mindful of SEO and accessibility, this website was made for National Louis University's coding challenge.",
         link : "https://mern-flavor-site.onrender.com/",
-        github : "https://github.com/ryan2625/NLU-flavor-app"},
+        github : "https://github.com/ryan2625/NLU-flavor-app"
+    },
     {
         image : './assets/boxing.webp',
         title : "Cross-City Boxing",
-        tech : "Javascript, HTML, CSS",
+        tech : ["React", "CSS", "Javascript"],
         description : "A react app created based on a Figma design.",
         link : "https://cross-city-boxing.vercel.app/",
-        github : "https://github.com/ryan2625/cross-city-boxing"},
+        github : "https://github.com/ryan2625/cross-city-boxing",
+        svg: "./assets/glove.svg",
+        svgClass:"top-right"
+    },
     {
         image : './assets/nexus.webp',
         title : "Nexus Retailers",
-        tech : "React, CSS, Javascript, Bootstrap",
+        tech : ["React", "CSS", "Javascript", "Bootstrap"],
         description : "A fully responsive react app created using bootstrap and the Intersection Observer API.",
         link : "https://nexus-cars.vercel.app/",
-        github : "https://github.com/ryan2625/nexus-Cars"},
+        github : "https://github.com/ryan2625/nexus-Cars",
+        svg: "./assets/car.svg",
+        svgClass: "top-left"
+    }
+   
+   /*
     {
         image : './assets/personal.webp',
         title : "Personal Website",
         tech : "React, Javascript, CSS",
         description : "One of my first web development projects. Creating this website was a special experience and my first attempt at building responsive websites from scratch. If you would like to get to know more about me and see more of my projects and research, visit here! ",
         link : "https://ryan-dev.vercel.app/",
-        github : "https://github.com/ryan2625/personalWebsite"},
+        github : "https://github.com/ryan2625/personalWebsite"}
+
+    */
 ]
--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,58 @@
 import { data } from "./data.js";
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     data.forEach((element, index) => {
         var img = $('<img>').attr('src', element.image).attr("loading", "lazy")
         var individual = $('<div>').addClass('individual').addClass("hidden");
-        var divider = $('<div>').addClass('portfolio-divider');
+        var individualPic = $('<div>').addClass('relative');
+        var divider = $('<div>').addClass('portfolio-divider')
         var desc = $('<div>').addClass('descriptions');
         var titles = $('<div>').addClass('titles');
         var h1 = $('<h1>').text(element.title);
-        var h2 = $('<h2>').text(element.tech);
         var p = $('<p>').text(element.description);
         var buttons = $('<div>').addClass('buttons');
         var a1 = $('<a>').attr('href', element.link).text('Live App').attr("target", "_blank");
         var a2 = $('<a>').attr('href', element.github).text('Github').attr("target", "_blank");
+        var techDiv = $('<div>').addClass('tech-div');
+
+        if (element.svg) {
+            console.log("ASDASD")
+            var svg = $('<img>').attr('src', element.svg).addClass(element.svgClass);
+            divider.append(svg);
+        }
+
+
         individual.attr('id', index)
-        individual.append(img)
-        individual.append(desc)
+        individualPic.append(img)
+
+        if (index % 2 == 0) {
+            individual.append(individualPic)
+            individual.append(desc)
+        } else {
+            individual.append(desc)
+            individual.append(individualPic)
+        }
+
+        
 
         desc.append(titles)
         desc.append(p)
         desc.append(buttons)
 
         titles.append(h1)
-        titles.append(h2)
+
+        element.tech.forEach((element, index) => {
+            var p = $('<p>').text(element);
+            techDiv.append(p)
+        })
+        titles.append(techDiv)
 
         buttons.append(a1)
         buttons.append(a2)
-
         $(".portfolio-display").append(individual);
-        $(".portfolio-display").append(divider)
-
+        $(".portfolio-display").append(divider);
     });
-
-    var img2 = $('<img>').attr('src', "./assets/dumbbell.svg")
-    var img3 = $('<img>').attr('src', "./assets/glove.svg")
-    var img4 = $('<img>').attr('src', "./assets/car.svg")
-
-    var div1 = $('<div>').attr('id', 'blob');
-    var div2 = $('<div>').attr('id', 'blob2');
-    var div3 = $('<div>').attr('id', 'blob4');
-
-    $("#0").append(div1.append(img2))
-    $("#1").append(div2.append(img3))
-    $("#2").append(div3.append(img4))
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
@@ -52,7 +61,7 @@ $(document).ready(function() {
             }
         });
     }, {
-        rootMargin: "0px 0px -250px 0px" 
+        rootMargin: "0px 0px -250px 0px"
     });
 
     const hiddenElements = document.querySelectorAll(".hidden");
@@ -60,11 +69,11 @@ $(document).ready(function() {
     hiddenElements.forEach(element => {
         observer.observe(element);
     });
-    
+
     setTimeout(() => {
         $(".loading").addClass("none");
         console.log("Hello fellow developers =)");
         $("html").css("overflow-y", "scroll");
     }, 2200);
 
-  });
+});
