21bf256b795843fcd27a964546288855041ff9a7
ryan2625 <ryanfreas19@augustana.edu>
2023-12-11 23:50:36 -0600

    Updating paths and file structure

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,16 +1,16 @@
 export const data = [
     {
-        image : './assets/cryptoVerse.webp',
+        image : './assets/images/cryptoVerse.webp',
         title : "crypto-verse",
         tech : ["React", "SCSS"],
         description : "A crypto app where you can view the latest market data for cryptocurrencies via CoinGecko's API. ",
         link : "https://crypto-net-phi.vercel.app/",
         github : "https://github.com/ryan2625/crypto-net",
-        svg: "./assets/bitcoin.svg",
+        svg: "./assets/SVGs/bitcoin.svg",
         svgClass:"top-right"
     },
     {
-        image : './assets/salesSpherePreview.webp',
+        image : './assets/images/salesSpherePreview.webp',
         title : "SalesSphere",
         tech : ["React", "CSS"],
         description : "A fully functional ecommerce website complete with many different products and features.",
@@ -18,7 +18,7 @@ export const data = [
         github : "https://github.com/ryan2625/simply-ecom"
     },
     {
-        image: "./assets/nighthawk.webp",
+        image: "./assets/images/nighthawk.webp",
         title: "Nighthawk Fitness",
         demo: {
             gmail: "demo_fitness@gmail.com",
@@ -28,11 +28,11 @@ export const data = [
         description: "A full-stack fitness app that can track your weight, create and log your workouts, and track your calories. This app is responsive and also includes user authentication and supports CRUD operations.",
         link: "https://fitness-dev-2.web.app/",
         github: "https://github.com/AugustanaCSC490Spring23/Nighthawk-Fitness",
-        svg: "./assets/dumbbell.svg",
+        svg: "./assets/SVGs/dumbbell.svg",
         svgClass: "top-left"
     },
     {
-        image : './assets/flavors.webp',
+        image : './assets/images/flavors.webp',
         title : "Flavor Store",
         tech : ["MERN Stack", "CSS"],
         description : "A fully responsive app that supports CRUD operations and dynamically renders items from the backend. Mindful of SEO and accessibility, this website was made for National Louis University's coding challenge.",
@@ -40,23 +40,23 @@ export const data = [
         github : "https://github.com/ryan2625/NLU-flavor-app"
     },
     {
-        image : './assets/boxing.webp',
+        image : './assets/images/boxing.webp',
         title : "Cross-City Boxing",
         tech : ["React", "CSS", "Javascript"],
         description : "A react app created based on a Figma design.",
         link : "https://cross-city-boxing.vercel.app/",
         github : "https://github.com/ryan2625/cross-city-boxing",
-        svg: "./assets/glove.svg",
+        svg: "./assets/SVGs/glove.svg",
         svgClass:"top-right"
     },
     {
-        image : './assets/nexus.webp',
+        image : './assets/images/nexus.webp',
         title : "Nexus Retailers",
         tech : ["React", "CSS", "Bootstrap"],
         description : "A fully responsive react app created using bootstrap and the Intersection Observer API.",
         link : "https://nexus-cars.vercel.app/",
         github : "https://github.com/ryan2625/nexus-Cars",
-        svg: "./assets/car.svg",
+        svg: "./assets/SVGs/car.svg",
         svgClass: "top-left"
     }
    
