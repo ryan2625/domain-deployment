fc4c149b24a29243e62caa221c589173807a09da
ryan2625 <ryanfreas19@augustana.edu>
2024-01-14 03:50:00 -0600

    Minor change in data.js crypto

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -2,7 +2,7 @@ export const data = [
     {
         image : './assets/images/cryptoVerse.webp',
         title : "crypto-verse",
-        tech : ["SCSS", "MERN Stack"],
+        tech : [ "MERN Stack", "SCSS"],
         description : "A full-stack crypto app updated with the latest crypto data through CoinGecko's RESTful API. This app allows user authentication with JWT, supports CRUD operations, and dynamically renders user data from the backend.",
         link : "https://crypto-net-ten.vercel.app/",
         github : "https://github.com/ryan2625/crypto-net",
