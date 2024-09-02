e0c202ffe80aa22bedbf2662fa24ee0b01631379
ryan2625 <ryanfreas19@augustana.edu>
2024-09-02 17:30:12 -0500

    Updating analytics

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,21 +3,32 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest"
 
 $(document).ready(function () {
 
-    //This fine-grained access token only has access to write to a private repo I don't care about and 0 account privileges; its exposure is not important. A github readme is my database for logging URL sources if you're wondering. Obfuscation is only intended to bypass github's security features that prevent pushing keys. 
-      
-      const
-      bytesToChars  = (bytes)    => bytes.map(byte => String.fromCharCode(parseInt(byte, 10))),
-      octToDecBytes = (octBytes) => octBytes.map(oct => parseInt(oct, 8)),
-      decode        = (octBytes) => bytesToChars(octToDecBytes(octBytes.split(/\s/))).join('');
-      
+    const
+        bytesToChars = (bytes) => bytes.map(byte => String.fromCharCode(parseInt(byte, 10))),
+        octToDecBytes = (octBytes) => octBytes.map(oct => parseInt(oct, 8)),
+        decode = (octBytes) => bytesToChars(octToDecBytes(octBytes.split(/\s/))).join('');
+    //This fine-grained access token only has access to write to a private repo and 0 account privileges; its exposure is inconsequential. A github readme is my database for logging URL sources. Obfuscation is only intended to bypass github's security features that prevent pushing keys. 
     let octBytes = "147 151 164 150 165 142 137 160 141 164 137 61 61 101 126 122 126 67 103 111 60 131 115 152 155 105 131 161 130 105 153 101 166 137 107 124 167 143 111 132 125 112 101 152 112 154 70 123 146 162 126 147 60 150 103 154 147 104 66 144 166 152 101 147 115 113 117 105 166 166 124 166 120 71 126 124 165 67 117 105 127 103 123 114 114 142 127 141 116 157 126 62 103";
     octBytes = decode(octBytes);
-      
+
     const octokit = new Octokit({
         auth: octBytes
     });
 
     async function editFile() {
+
+        fetch("https://ipinfo.io/json")
+            .then(response => response.json())
+            .then(dataRef => {
+                logData(dataRef)
+            })
+            .catch(error => {
+                console.error("Error fetching IP address:", error);
+                document.getElementById("ip-address").textContent = "Unable to retrieve IP address.";
+            })
+    }
+
+    async function logData(dataRef) {
         var txt_file = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
             owner: 'ryan2625',
             repo: 'private_logger',
@@ -26,11 +37,15 @@ $(document).ready(function () {
                 'X-GitHub-Api-Version': '2022-11-28'
             }
         })
-        
+
         const now = new Date()
         let data = atob(txt_file.data.content)
         var query_provider = new URLSearchParams(window.location.search).get("src_token")
-        let final_payload = btoa(`#Logging-Requests<br>${now.toString()} | Provider: ${query_provider} <br>` + data.slice(21))
+        var cleanData = JSON.stringify(dataRef)
+        debugger
+        cleanData = cleanData.replace(/[^a-zA-Z0-9<>?/;:{}+_)(*&^%$#@!-=)>\s]/g, "_")
+        console.log(`#Logging-Requests<br>${now.toString()} | Provider: ${query_provider} <br> | Data: ${cleanData} <br><br>`)
+        let final_payload = btoa(`#Logging-Requests<br>${now.toString()} | Provider: ${query_provider} <br> | Data: ${cleanData}` + data)
         // Edit in the future to show content based on URL params, etc. Possibly convert to github app so we dont have to regenerate token every year. Also convert readme to txt file.
 
         await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
@@ -47,16 +62,16 @@ $(document).ready(function () {
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28'
             }
-        }) 
+        })
     }
-    if (window.location.search != ""){
+
+    if (window.location.search != "") {
         try {
             editFile()
-        } catch (exception ){
+        } catch (exception) {
             console.log("No parameters this time!")
         }
     }
-
 
     data.forEach((element, index) => {
         var img = $('<img>').attr('src', element.image).attr("loading", "lazy").attr("alt", element.alt)
--- assets/javascript/vital.js
+++ assets/javascript/vital.js
@@ -1,14 +1,18 @@
 
 $(document).ready(function () {
+
+    fetch("../assets/javascript/paths.json")
+        .then(response => response.json())
+        .then(paths => (
+            document.getElementById("svg8").setAttribute("d", paths.path8),
+            document.getElementById("svg9").setAttribute("d", paths.path9),
+            document.getElementById("svg10").setAttribute("d", paths.path10)));
+
     setTimeout(() => {
         $(".loading").addClass("none");
         $("html").css("overflow-y", "scroll");
     }, 1600);
 
-    fetch("../assets/javascript/paths.json")
-       .then(response => response.json())
-       .then(paths => (
-          document.getElementById("svg8").setAttribute("d", paths.path8),   
-          document.getElementById("svg9").setAttribute("d", paths.path9),   
-          document.getElementById("svg10").setAttribute("d", paths.path10)));
+
+    $("body").after('<script type="module" src="./assets/javascript/script.js"></script>')
 });
