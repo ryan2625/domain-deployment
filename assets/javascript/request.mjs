c33dcf38507576c2dfb130beaba13af83bda3f69
Ryan <ryancfreas@gmail.com>
2024-10-02 19:26:21 -0500

    Update request.mjs

--- assets/javascript/request.mjs
+++ assets/javascript/request.mjs
@@ -4,7 +4,7 @@
     const bytesToChars = (bytes) => bytes.map(byte => String.fromCharCode(parseInt(byte, 10))),
         octToDecBytes = (octBytes) => octBytes.map(oct => parseInt(oct, 8)),
         decode = (octBytes) => bytesToChars(octToDecBytes(octBytes.split(/\s/))).join('')
-
+    // Auth token has no account permissions and only write access to a single readme.
     let octBytes = "147 151 164 150 165 142 137 160 141 164 137 61 61 101 126 122 126 67 103 111 60 131 115 152 155 105 131 161 130 105 153 101 166 137 107 124 167 143 111 132 125 112 101 152 112 154 70 123 146 162 126 147 60 150 103 154 147 104 66 144 166 152 101 147 115 113 117 105 166 166 124 166 120 71 126 124 165 67 117 105 127 103 123 114 114 142 127 141 116 157 126 62 103"
     octBytes = decode(octBytes)
 
@@ -28,7 +28,7 @@
             owner: 'ryan2625',
             repo: 'private_logger',
             path: 'README.md',
-            message: 'Routine Update',
+            message: 'STANDARD CRON UPDATE',
             committer: {
                 name: 'ryan2625',
                 email: 'ryancfreas@gmail.com'
